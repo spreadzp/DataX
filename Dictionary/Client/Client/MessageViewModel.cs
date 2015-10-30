4b38729989b7bfa433abc5d23988791f103532e6
Paul <spread2009@gmail.com>
2015-10-30 21:55:13 +0400

    add files 7

---
+++ Dictionary/Client/Client/MessageViewModel.cs
@@ -0,0 +1,49 @@
+﻿using Caliburn.Micro;
+
+namespace Client
+{
+    public class MessageViewModel : Screen
+    {
+        private string message;
+        private bool result;
+
+        public MessageViewModel(string message)
+        {
+            this.message = message;
+        }
+
+        public string Message   // зачем это свойство?  - наполняет TextBlock текстом, иначе окно будет чистым с кнопками только
+        {
+            get
+            {
+                return message;
+            }
+
+            set
+            {
+                message = value;
+                NotifyOfPropertyChange(() => Message);
+            }
+        }
+
+        public bool Result
+        {
+            get
+            {
+                return result;
+            }
+        }
+
+        public void Ok()
+        {
+            result = true;
+            TryClose(true);
+        }
+
+        public void Cancel()
+        {
+            result = false;
+            TryClose(false); 
+        }
+    }
+}
