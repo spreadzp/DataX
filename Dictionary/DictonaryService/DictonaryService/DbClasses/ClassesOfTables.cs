fb480076e39f8445412f5b7921633763980a0bd6
Paul <spread2009@gmail.com>
2015-10-30 21:53:17 +0400

    add files 5

---
+++ Dictionary/DictonaryService/DictonaryService/DbClasses/ClassesOfTables.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using DictonaryService.DbClasses;
+
+namespace DictonaryService
+{
+    public class RusDictonary : Dictonary
+    {
+        public virtual ICollection<EngDictonary> Words
+        {
+            get;
+            set;
+        }
+    }
+
+    public class EngDictonary : Dictonary
+    {
+        public virtual ICollection<RusDictonary> Words
+        {
+            get;
+            set;
+        }
+    }
+
+    public class RelationsTable
+    {
+        public int Id 
+        { 
+            get;
+            set;
+        }
+
+        public virtual int IdEngWord
+        { 
+            get;
+            set;
+        }
+
+        public virtual int IdRusWord 
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ Dictionary/DictonaryService/DictonaryService/DbClasses/DictionaryContext.cs
@@ -0,0 +1,44 @@
+﻿using System.Data.Common;
+using System.Data.Entity;
+
+namespace DictonaryService
+{
+    public class DictonaryContext : DbContext 
+    {
+       public DictonaryContext()
+            : base("DbConnection")
+       {
+       }
+
+       public DictonaryContext(DbConnection connection)
+               : base(connection, true)
+       {
+           this.Configuration.LazyLoadingEnabled = false;
+       }
+
+       public DbSet<RusDictonary> RusDictonaries
+       {
+           get;
+           set;
+       }
+
+       public DbSet<EngDictonary> EngDictonaries
+       {
+           get;
+           set;
+       }
+
+       protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+          modelBuilder.Entity<RusDictonary>()
+                    .HasMany<EngDictonary>(s => s.Words)
+                    .WithMany(c => c.Words)
+                    .Map(cs =>
+                    {
+                        cs.MapLeftKey("RusDictionaryRefId");
+                        cs.MapRightKey("EngDictionaryRefId");
+                        cs.ToTable("RelationsTables");
+                    });
+        }
+    }
+}
