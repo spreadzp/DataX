7686f8e45327f838c11aa31af50781225d44e49f
Paul <spread2009@gmail.com>
2015-10-30 21:54:22 +0400

    add files 6

---
+++ Dictionary/DictonaryService/DictonaryService/DbClasses/Dictonary.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictonaryService.DbClasses
+{
+    public class Dictonary  
+    {
+        public int Id
+        {
+            get;
+            set;
+        }
+
+        public string Word
+        {
+            get;
+            set;
+        }
+
+        public virtual ICollection<Dictonary> Words
+        {
+            get;
+            set;
+        }
+    }
+}
