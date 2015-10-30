989b2ca6470d72d4ccb662ff2ceb0d2e44dda711
Paul <spread2009@gmail.com>
2015-10-30 21:49:31 +0400

    add files 2

---
+++ Dictionary/EnglishRusDictonary/UnitTestDictonary/UnitTestClient.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestDictonary
+{
+    [TestClass]
+    public class UnitTestClient
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Dictionary/EnglishRusDictonary/UnitTestDictonary/UnitTestDictonaryServise.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ServiceDictionary;
+
+namespace UnitTestDictonary
+{
+    [TestClass]
+    public class UnitTestDictonaryServise
+    {
+        [TestMethod]
+        public void TestFindTranslate()
+        {
+            ServiceDictonary service  = new ServiceDictonary();
+            Assert.Equals()
+        } 
+
+         [TestMethod]
+        public void TestMethod1()
+        {
+             
+        } 
+    }
+    }
+}
